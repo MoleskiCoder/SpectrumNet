9e246b684116cbbf29324fe79b552616ec6242bd
Adrian Conlon <Adrian.conlon@gmail.com>
2019-12-03 21:31:11 +0000

    Whoops: Missed addition of SteppingEventArgs.cs!
    Signed-off-by: Adrian Conlon <Adrian.conlon@gmail.com>

---
+++ SpectrumNet/SteppingEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace SpectrumNet
+{
+    using System;
+
+    public class SteppingEventArgs : EventArgs
+    {
+        public SteppingEventArgs(int cycles) => this.Cycles = cycles;
+
+        public int Cycles { get; } = 0;
+    }
+}

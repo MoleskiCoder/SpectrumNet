6ff579f811459cb97a0cc7dd78fdedb68fd9d5a0
Adrian Conlon <98398945+AdrianConlon@users.noreply.github.com>
2025-01-27 21:23:47 +0000

    Simplify i/o port handling in Z80 implementation

--- SpectrumNet/Cabinet.cs
+++ SpectrumNet/Cabinet.cs
@@ -100,7 +100,7 @@
             this.DrawPixels();
         }
 
-        protected override void OnExiting(object sender, EventArgs args)
+        protected override void OnExiting(object sender, ExitingEventArgs args)
         {
             base.OnExiting(sender, args);
             this.Motherboard.LowerPOWER();

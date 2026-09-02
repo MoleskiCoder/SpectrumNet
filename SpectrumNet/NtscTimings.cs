aadefde8bb97049b4cc23dcd3ca2c43d677ea5af
Adrian Conlon <Adrian.conlon@gmail.com>
2026-09-02 17:27:55 +0100

    Correct NTSC bottom border.  (Fixes NTSC FPS calculation)

--- SpectrumNet/NtscTimings.cs
+++ SpectrumNet/NtscTimings.cs
@@ -6,7 +6,7 @@
         public int RightRasterBorder { get; } = 64;
 
         public int TopRasterBorder { get; } = 32;
-        public int BottomRasterBorder { get; } = 24;
+        public int BottomRasterBorder { get; } = 32;
 
         public float MasterClockRate { get; } = 14_110_000.0f;
     }
